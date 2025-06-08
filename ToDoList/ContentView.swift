ddc2829afb0424dec6f06918faeecc9dcf9009fd
Bhaskara Padala <vamsikrishna2002@icloud.com>
2025-06-07 20:06:48 -0700

    created

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, To Do List with Firebase")
+            Text("Hello, To do List with Firebase")
         }
         .padding()
     }
